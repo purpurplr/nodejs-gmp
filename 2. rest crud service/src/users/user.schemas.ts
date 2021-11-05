243777a48ac7392a18568a13a93f05fe88719aa4
Aliaksei Auramionak <aliaksei_auramionak2@epam.com>
2021-11-05 15:08:40 +0300

    feat: 3.2 postgres layered

--- 2. rest crud service/src/users/user.schemas.ts
+++ 2. rest crud service/src/users/user.schemas.ts
@@ -1,22 +1,10 @@
 import Joi from 'joi';
 import { passwordRegEx } from '../shared/regex';
 
-export interface User {
-  id: string;
-  login: string;
-  password: string;
-  age: number;
-  isDeleted: boolean;
-}
-
-export type UserDraft = Omit<User, 'id' | 'isDeleted'>;
-
-const MIN_AGE = 4;
-const MAX_AGE = 130;
 export const userDraftSchema = Joi.object({
-  login: Joi.string(),
-  password: Joi.string().regex(passwordRegEx),
-  age: Joi.number().min(MIN_AGE).max(MAX_AGE),
+  login: Joi.string().min(4).max(20),
+  password: Joi.string().regex(passwordRegEx).max(50),
+  age: Joi.number().min(4).max(130),
 }).min(1);
 
 export const suggestedUsersSchema = Joi.object({
